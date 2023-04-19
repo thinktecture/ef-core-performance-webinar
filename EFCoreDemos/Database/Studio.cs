6cfd514ea033878737f8b1013c53167be7df0996
Pawel Gerr <pawel.gerr@thinktecture.com>
2023-04-19 12:08:09 +0200

    Products must be virtual for lazy loading

--- EFCoreDemos/Database/Studio.cs
+++ EFCoreDemos/Database/Studio.cs
@@ -5,5 +5,5 @@ public class Studio
    public int Id { get; set; }
    public string Name { get; set; }
 
-   public List<Product> Products { get; set; }
+   public virtual List<Product> Products { get; set; }
 }
